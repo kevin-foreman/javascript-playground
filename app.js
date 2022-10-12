9d6fd4363adf21bd3cfb1b19c8b8f490f455e567
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-12 14:43:18 -0500

    Starting to push changes after each block so I have some reference material

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-window.onload = () => {
-    console.log('This is your first program executing!');
-}
+// .splice() *This is one of several different ways to use splice(), read the documentation
+var lunchItems = ['Turkey Sandwich', 'Falafel', 'Paella', 'Processor Speed', 'RAM', 'Gyro'];
+var indexOfFirstItemToBeRemoved = 3;
+var numberOfItemsToRemove = 2;
+var itemToBeAdded1 = 'Fish Tacos';
+var itemToBeAdded2 = 'Pizza';
+
+lunchItems.splice(indexOfFirstItemToBeRemoved, numberOfItemsToRemove, itemToBeAdded1, itemToBeAdded2);
+console.log('list of lunch items after splicing:', lunchItems);
+// Logs [ 'Turkey Sandwich', 'Falafel', 'Paella', 'Fish Tacos', 'Pizza', 'Gyro' ]
