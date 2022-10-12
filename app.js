cab059a621efcf9b4468db918c18e4be307050b4
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-12 14:56:34 -0500

    Concat method for arrays displayed

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-function applySlice(array, start, end) {
-    // create a sliceOfArray variable
-    // assign it to a portion of the array from before start, up to, but not including end
-    const sliceOfArray = array.slice(start, end);
-    // return the sliceOfArray variable
-    return sliceOfArray;
+function applyConcat(array1, array2) {
+    // create a concattedArray variable
+    // assign it to the contents of both array1 and array2
+    const catArr = array1.concat(array2);
+    // return the concattedArray variable
+    return catArr;
 };
 
-var array1 = ['Q', 'u', 'e', 'u', 'e'];
-var resultOfSlice1 = applySlice(array1, 1, 4);
-console.log('should log ["u", "e", "u"]:', resultOfSlice1);
+var array1 = ['a', 'b', 'c'];
+var array2 = ['d', 'e', 'f']
+var resultConcat1 = applyConcat(array1, array2);
+console.log('should log ["a", "b", "c", "d", "e", "f"]:', resultConcat1);
 
-var array2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var resultOfSlice2 = applySlice(array2, 2, 8);
-console.log('should log [2, 3, 4, 5, 6, 7]:', resultOfSlice2);
+var array3 = [1, 2, 3];
+var array4 = [4, 5, 6];
+var resultConcat2 = applyConcat(array3, array4);
+console.log('should log [1, 2, 3, 4, 5, 6]:', resultConcat2);
