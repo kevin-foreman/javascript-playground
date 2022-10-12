5a74163410c0887ec4de1c761f7c5367e0fb9831
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-12 15:02:31 -0500

    IndexOf method displayed

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-function applyConcat(array1, array2) {
-    // create a concattedArray variable
-    // assign it to the contents of both array1 and array2
-    const catArr = array1.concat(array2);
-    // return the concattedArray variable
-    return catArr;
-};
+function applyIndexOfToArray(array, element) {
+    // create an index variable
+    // assign it to the index inside of array where element can be found
+    const index = array.indexOf(element);
+    // return the index variable
+    return index;
+};  
 
-var array1 = ['a', 'b', 'c'];
-var array2 = ['d', 'e', 'f']
-var resultConcat1 = applyConcat(array1, array2);
-console.log('should log ["a", "b", "c", "d", "e", "f"]:', resultConcat1);
+var array1 = ['Quick', 'sort', 'is', 'wild'];
+var element1 = 'sort';
+var resultIndex1 = applyIndexOfToArray(array1, element1);
+console.log('should log 1:', resultIndex1);
 
-var array3 = [1, 2, 3];
-var array4 = [4, 5, 6];
-var resultConcat2 = applyConcat(array3, array4);
-console.log('should log [1, 2, 3, 4, 5, 6]:', resultConcat2);
+var array2 = ['size', 'color', 'cut', 'price'];
+var element2 = 'style';
+var resultIndex2 = applyIndexOfToArray(array2, element2);
+console.log('should log -1:', resultIndex2);
