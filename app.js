96c54a2c248b6f29ba6e1a777f054767604652a7
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-21 13:47:52 -0500

    Using an object to count elements

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-function computeSumOfAllElements(numbers) {
-    // if input is empty, what do we return? (edge case)
-    if (numbers.length === 0) {
-        return 0;
+function countWords(stringOfWords) {
+    // your code here
+    // if input is an empty string
+    if (stringOfWords === '') {
+        // return empty object
+        return {};
     }
-    // output is the sum of all the numbers
-    var sumOfNumbers = 0;
-
-    // iterate over the entire input array
-    for (var i = 0; i < numbers.length; i++) {
-        // check if current number is greater than zero
-        if (numbers[i] > 0) {
-            // add numbers together
-            sumOfNumbers = sumOfNumbers + numbers[i];
+    // create result count object
+    var counts = {};
+    // split the input string into an array of words
+    var words = stringOfWords.split(' ');
+    // iterate over the array of words
+    for (var i = 0; i < words.length; i++) {
+        var currentWord = words[i];
+    // check if current word is not in result object
+        if (counts[currentWord] === undefined) {
+        // if not, instantiate current word in object with value of 1
+        counts[currentWord] = 1;
+    // otherwise
+        } else {
+        // increment value of current word in object by 1
+        counts[currentWord]++;
         }
+    };
 
-    }
 
-    // return result variable
-    return sumOfNumbers;
+    // return the result count object
+    return counts;
 };
 
-var result1 = computeSumOfAllElements([1, 2, 3]);
-console.log('should log 6:', result1);
+var result1 = countWords('ask a bunch get a bunch');
+console.log('should log "{ask: 1, a: 2, bunch: 2, get: 1}":', result1);
 
-var result2 = computeSumOfAllElements([]);
-console.log('should log 0:', result2);
+var result2 = countWords('');
+console.log('should log "{}":', result2);
