2396ae27a50533754160353a7354356de5764323
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-11-10 08:55:38 -0600

    Keep elements from a selected array with an argument using .filter method

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-function removeElement(array, discarder) {
+function keep(array, keeper) {
     // your code here
     /* START SOLUTION */
 
     return array.filter(function (numbersToKeep) {
-        return numbersToKeep !== discarder;
+        return numbersToKeep === keeper;
     });
 
     /* END SOLUTION */
 };
 
-var output = removeElement([1, 2, 3, 2, 1], 2);
-console.log(output); // --> [1, 3, 1]
+var output = keep([1, 2, 3, 2, 1], 2)
+console.log(output); // --> [2, 2]
