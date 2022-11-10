3dfe8be3df82a4bfc0b553a9a4c7450e647f6cc3
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-11-10 10:45:54 -0600

    clean slate

--- sample.js
+++ sample.js
@@ -1,1 +1,28 @@
-console.log(("b" + "a" + + "a" + "a").toLowerCase())
+function getOddLengthWordsAtProperty(obj, key) {
+    // your code here
+    /* START SOLUTION */
+
+    if (Array.isArray(obj[key]) === false || obj[key].length === 0) {
+
+        return [];
+
+    }
+
+    var oddLengthWords = obj[key].filter(function (x) {
+        return x.length % 2 !== 0
+    })
+
+    return oddLengthWords;
+
+    /* END SOLUTION */
+};
+
+var obj = {
+    key: ['It', 'has', 'some', 'words']
+};
+var output = getOddLengthWordsAtProperty(obj, 'key');
+console.log(output); // --> ['has', 'words']
+
+
+
+// console.log(("b" + "a" + + "a" + "a").toLowerCase())
