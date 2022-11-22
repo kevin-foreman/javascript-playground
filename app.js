5acc0d015fd93c141666f239caf7db5674e132eb
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-11-22 11:48:02 -0600

    Return a single array from an input of several arrays

---
+++ app.js
@@ -0,0 +1,11 @@
+function joinArrayOfArrays(arr) {
+    // your code here
+
+    var joinedArr = [].concat.apply([], arr);
+
+    return joinedArr;
+
+};
+
+var output = joinArrayOfArrays([[1, 4], [true, false], ['x', 'y']]);
+console.log(output); // --> [1, 4, true, false, 'x', 'y']
