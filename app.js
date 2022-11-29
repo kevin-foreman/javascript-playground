2999d6e192189f5d907654d89871c20aaae450da
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-11-29 12:36:38 -0600

    Transform an input array into an object with key value pairs being the array's elements

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-function transformFirstAndLast(array) {
+function fromListToObject(array) {
   // your code here
-  var transformed = {};
-  transformed[array[0]] = array[array.length -1];
-  return transformed
+  var newObject = {};
+  for (var i = 0; i < array.length; i++) {
+    newObject[array[i][0]] = array[i][1];
+  }
+  return newObject;
 };
 
-var output = transformFirstAndLast(['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Foreman']);
-console.log(output); // ==> { Kevin : Foreman }
+var output = fromListToObject([['make', 'Toyota'], ['model', 'Tundra'], ['year', 2023]]);
+console.log(output); // ==> { make : 'Toyota', model : 'Tundra', year : 2023 }
