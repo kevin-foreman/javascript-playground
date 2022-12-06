281426bfe60e2f28ba25e4c2c291bfe003197173
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-12-06 12:51:32 -0600

    Logger function to apply a function to a string

---
+++ app.js
@@ -0,0 +1,24 @@
+function logger(func, str) {
+    //Your psuedocode here
+    // Create an empty string to store the string we plan to modify with the given function
+    // Iterate over the string to determine what it consists of
+    // Create a variable consisting of the parts of the string
+    // Apply the function to each letter in the string and concatenate the results
+    // return the resulting string
+
+    //Your code here
+    var myString = "";
+    for (var i = 0; i < str.length; i++) {
+        myString += (func(str[i]));
+    }
+    return myString;
+};
+
+function test (char) {
+    char += "Z";
+    return char;
+    // console.log(char);
+};
+
+var output = logger(test, 'Hello');
+console.log(output);
